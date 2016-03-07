523eb513eca6d057e986cf259e0e007bab691e40
Kevin Van Robbroeck <kevin.van.robbroeck@gmail.com>
2016-03-07 16:18:18 +0100

    Added merge of projects. Not ready for push.

---
+++ web-module/src/main/java/com/realdolmen/tickets/beans/PersonBean.java
@@ -0,0 +1,18 @@
+package com.realdolmen.tickets.beans;
+
+import com.realdolmen.tickets.domain.Person;
+import com.realdolmen.tickets.repository.PersonRepositoryBean;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import java.util.List;
+
+@ManagedBean
+public class PersonBean {
+    @EJB
+    private PersonRepositoryBean personRepository;
+
+    public List<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
+}
