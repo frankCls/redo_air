8b5007cd92e712c0d10256fa755476b1334baf7c
timo <timothy.leduc@realdolmen.com>
2016-08-31 11:37:27 +0200

    added getters/setters/annotations in classes: Booking, CreditCard, Employee, Ticket, Payer

--- src/main/java/com/redoair/domain/Booking.java
+++ src/main/java/com/redoair/domain/Booking.java
@@ -1,12 +1,73 @@
 package com.redoair.domain;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
-public class Booking {
+import java.util.Set;
 
-	private List<Ticket> tickets;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
+import com.sun.istack.NotNull;
+
+@Entity
+public class Booking  implements Serializable{
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@OneToMany( cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, fetch = FetchType.EAGER)
+   	private Set<Ticket> tickets;
+
+	@Temporal(TemporalType.DATE)
 	private Date dateBooked;
 
+	@NotNull
+	@ManyToOne
 	private Payer payer;
+
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Set<Ticket> getTickets() {
+		return tickets;
+	}
+
+	public void setTickets(Set<Ticket> tickets) {
+		this.tickets = tickets;
+	}
+
+	public Date getDateBooked() {
+		return dateBooked;
+	}
+
+	public void setDateBooked(Date dateBooked) {
+		this.dateBooked = dateBooked;
+	}
+
+	public Payer getPayer() {
+		return payer;
+	}
+
+	public void setPayer(Payer payer) {
+		this.payer = payer;
+	}
+	
+	
 }
--- src/main/java/com/redoair/domain/Payer.java
+++ src/main/java/com/redoair/domain/Payer.java
@@ -1,10 +1,67 @@
 package com.redoair.domain;
 
-public class Payer {
-	
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.sun.istack.NotNull;
+
+@Entity
+public class Payer implements Serializable {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@NotNull
 	private String firstName;
-	
+	@NotNull
 	private String lastName;
-	
-	private CreditCard creditCard;
+
+	@ElementCollection(fetch = FetchType.LAZY)
+	@CollectionTable(name = "CreditCardList")
+	private List<CreditCard> creditCard;
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<CreditCard> getCreditCard() {
+		return creditCard;
+	}
+
+	public void setCreditCard(List<CreditCard> creditCard) {
+		this.creditCard = creditCard;
+	}
+
 }
